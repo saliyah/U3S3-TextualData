1235884d9c8e071c2b2ec913d7bb6d603549acff
saliyah <saliyah14@outlook.com>
2024-12-12 15:08:12 -0500

    Update ShoppingCart01.java

--- src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
+++ src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
@@ -1,14 +1,26 @@
-package com.codedifferently.labs.partA;
-
 public class ShoppingCart01 {
     public static void main(String[] args) {
         // Declare and initialize String variables.  Do not initialize message yet.
-
+        //int custName = "Alex";
+        //int itemDesc = "Shirts";
+        String custName = "Alex";
+        String itemDesc = "Shirts";
+        String message = custName + "wants to purchase a shirt" + itemDesc;
 
 
 
         // Assign the message variable
+        double price = 19.13;
+        double tax = .0065;
+        int quantity = 2;
 
+        String totalPrice = "$" + (price * tax) + price;
+
+        String message1 = custName + "wants to purchase" + quantity +
+                String message2 =
+
+                System.out.println(message1);
+        System.out.println(message2);
 
         // Print and run the code
 
