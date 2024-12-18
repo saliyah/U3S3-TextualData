1f5006fcc157ab4804333ada33390bf6f43924c5
saliyah <saliyah14@outlook.com>
2024-12-18 17:00:33 -0500

    Datatypes
    i was having trouble creating a new repository for datatypes, so I just put it under this one. I will work on getting it under the right repository.

--- src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
+++ src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
@@ -1,4 +1,4 @@
-//package com.codedifferently.labs.partA;
+package com.codedifferently.labs.partA;//package com.codedifferently.labs.partA;
 
 //public class ShoppingCart01 {
 //public static void main(String[] args) {
@@ -16,3 +16,31 @@
 
    // }
 //}
+   public class Casting01 {
+       public static void main(String[] args) {
+           //Declare and initialize a byte with a value of 128
+           //Observe NetBeans' complaint
+
+           short s1 = 128;
+         
+           //Declare and initialize a short with a value of 128
+           //Create a print statement that casts this short to a byte;
+           byte b2 = (byte) s1;
+           System.out.println (" short value (128) cast to byte: " + b2);
+
+
+           //Declare and initialize a byte with a value of 127
+           //Add 1 to this variable and print it
+           //Add 1 to this variable again and print it again
+
+           int b3 = 128;
+           b3++;
+           System.out.println ("After adding 1 to 127: " + b3);
+
+           b3++;
+           System.out.println ("After adding 1 to 127: " + b3);
+
+
+       }
+   }
+
--- src/main/java/com/codedifferently/labs/partA/ShoppingCart02.java
+++ src/main/java/com/codedifferently/labs/partA/ShoppingCart02.java
@@ -1,25 +1,25 @@
 package com.codedifferently.labs.partA;
 
-public class ShoppingCart02 {
-    public static void main(String[] args) {
-        String custName = "Alex";
-        String itemDesc = "Shirts";
+//public class ShoppingCart02 {
+    //public static void main(String[] args) {
+        //String custName = "Alex";
+        //String itemDesc = "Shirts";
 
-        System.out.println("Alex");
-        System.out.println("Shirt");
+        //System.out.println("Alex");
+        //System.out.println("Shirt");
 
         // Declare and initialize numeric fields: price, tax, quantity.
         // Assign the message variable
-        double price = 12.8870;
-        double tax = 0.0065;
-        int quantity = 2;
-        double totalPrice = (price * quantity) + tax;
+        //double price = 12.8870;
+        //double tax = 0.0065;
+        //int quantity = 2;
+        //double totalPrice = (price * quantity) + tax;
 
-        String message1 = custName + " wants to purchase " + quantity + " " + itemDesc;
-        String message2 = "The total cost with tax is " + totalPrice;
+        //String message1 = custName + " wants to purchase " + quantity + " " + itemDesc;
+        //String message2 = "The total cost with tax is " + totalPrice;
 
-        System.out.println(message1);
-        System.out.println(message2);
+        //System.out.println(message1);
+        //System.out.println(message2);
 
 
 
