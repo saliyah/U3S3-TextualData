8e9c5a748c3fd732c37708a96e07cae4e0f1953b
saliyah <saliyah14@outlook.com>
2024-12-18 14:20:42 -0500

    12/18

--- src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
+++ src/main/java/com/codedifferently/labs/partA/ShoppingCart01.java
@@ -1,29 +1,18 @@
-public class ShoppingCart01 {
-    public static void main(String[] args) {
+//package com.codedifferently.labs.partA;
+
+//public class ShoppingCart01 {
+//public static void main(String[] args) {
         // Declare and initialize String variables.  Do not initialize message yet.
-        //int custName = "Alex";
-        //int itemDesc = "Shirts";
-        String custName = "Alex";
-        String itemDesc = "Shirts";
-        String message = custName + "wants to purchase a shirt" + itemDesc;
+        //String custName = "Alex";
+        //String itemDesc = "Shirts";
+        //String message = custName + " wants to purchase a " + itemDesc;
 
-
-
-        // Assign the message variable
-        double price = 19.13;
-        double tax = .0065;
-        int quantity = 2;
-
-        String totalPrice = "$" + (price * tax) + price;
-
-        String message1 = custName + "wants to purchase" + quantity + "" +itemDesc;
-        String message2 = "The total cost with tax is" + totalPrice;
-
-        System.out.println(message1);
-        System.out.println(message2);
+        //System.out.println("Alex");
+        //System.out.println("Shirt");
+        //System.out.println(message);
 
 
         // Print and run the code
 
-    }
-}
+   // }
+//}
--- src/main/java/com/codedifferently/labs/partA/ShoppingCart02.java
+++ src/main/java/com/codedifferently/labs/partA/ShoppingCart02.java
@@ -4,9 +4,22 @@ public class ShoppingCart02 {
     public static void main(String[] args) {
         String custName = "Alex";
         String itemDesc = "Shirts";
-        String message = custName+" wants to purchase a "+itemDesc;
+
+        System.out.println("Alex");
+        System.out.println("Shirt");
 
         // Declare and initialize numeric fields: price, tax, quantity.
+        // Assign the message variable
+        double price = 12.8870;
+        double tax = 0.0065;
+        int quantity = 2;
+        double totalPrice = (price * quantity) + tax;
+
+        String message1 = custName + " wants to purchase " + quantity + " " + itemDesc;
+        String message2 = "The total cost with tax is " + totalPrice;
+
+        System.out.println(message1);
+        System.out.println(message2);
 
 
 
@@ -15,7 +28,6 @@ public class ShoppingCart02 {
 
         // Modify message to include quantity
 
-        System.out.println(message);
 
         // Print another message with the total cost
 
